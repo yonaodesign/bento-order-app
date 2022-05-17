ef3aa77d2a44473b0b3b114f3f800a66a2f67f6b
jonas <52710606+yonaodesign@users.noreply.github.com>
2022-05-18 00:37:06 +0900

    quick update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import usersDB from './userDB';
 import bentoOrdersDB from './bentoOrdersDB'
 import AdminDisplayTodayOrders from './components/AdminDisplayTodayOrders';
+import styled from 'styled-components'
 
 function App() {
   const [bentoOrders, setBentoOrders] = useState(bentoOrdersDB)
@@ -12,11 +13,24 @@ function App() {
   const [trigger, setTrigger] = useState(false)
   const myResult = bentoOrders.filter(order => order.userId == loggedUser.loggedUserId)
 
+  const SwitcherBar = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 10px 10px;`
+
+  const PrettyButton = styled.button`
+  padding: 10px;`
+
   return (
     <div className="App">
       <header className="App-header">
         <Navbar name={loggedUser} />
-          <div>{usersDB.map((item, i) => <button key={i} onClick={()=>setLoggedUser({loggedUserId: item.userId, displayName: item.displayName})}>{item.userId}{' '}{item.displayName}</button>)}<button onClick={()=>setTrigger(!trigger)}>管理</button></div>
+          <SwitcherBar>
+            {
+            usersDB.map((item, i) => <PrettyButton key={i} onClick={()=>setLoggedUser({loggedUserId: item.userId, displayName: item.displayName})}>{item.userId}{' '}{item.displayName}</PrettyButton>)
+            }
+            <PrettyButton onClick={()=>setTrigger(!trigger)}>本日の注文管理</PrettyButton></SwitcherBar>
         <Calendar bentoOrders={bentoOrders} stateChanger={setBentoOrders} users={usersDB} name={loggedUser.loggedUserId} value={myResult}/>
         {trigger && <AdminDisplayTodayOrders bentoOrders={bentoOrders} names={usersDB}/>}
       </header>
